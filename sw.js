d8b8878d4976b8e39444bfde208e27598346ed28
lavergnes2005 <lavergnes2005@gmail.com>
2026-07-03 09:32:02 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ledger-budget-v2';
+const CACHE = 'ledger-budget-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e=>{
